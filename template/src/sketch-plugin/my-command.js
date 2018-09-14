d3b59054fa59879540c7a73b73e468a97f4e9f77
mathieudutour <mathieu@dutour.me>
2018-09-14 16:56:43 -0400

    render symbols instead of layers

--- template/src/sketch-plugin/my-command.js
+++ template/src/sketch-plugin/my-command.js
@@ -1,22 +1,8 @@
 import * as React from 'react'
-import { render, Artboard } from 'react-sketchapp'
+import { makeSymbol, injectSymbols } from 'react-sketchapp'
 import ButtonExamples from '../design-system/components/Button.examples'
 
-const Document = ({children}) => (
-  <Artboard
-    name="Buttons"
-    style={{
-      flexDirection: 'row',
-      flexWrap: 'wrap',
-      width: (150 + 8) * 2,
-    }}
-  >
-    {children}
-  </Artboard>
-);
-
 export default function(context) {
-  render(<Document>
-    {ButtonExamples.map(e => e.component)}
-  </Document>, context.document.currentPage());
+  ButtonExamples.forEach(e => makeSymbol(() => e.component, e.name))
+  injectSymbols(context.document)
 }
