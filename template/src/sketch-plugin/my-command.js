d078d80815948f844fb24f8b9d70b903b24bec95
mathieudutour <mathieu@dutour.me>
2018-09-14 17:07:57 -0400

    generate library

--- template/src/sketch-plugin/my-command.js
+++ template/src/sketch-plugin/my-command.js
@@ -1,8 +1,25 @@
 import * as React from 'react'
 import { makeSymbol, injectSymbols } from 'react-sketchapp'
+import sketch from 'sketch'
 import ButtonExamples from '../design-system/components/Button.examples'
 
 export default function(context) {
-  ButtonExamples.forEach(e => makeSymbol(() => e.component, e.name))
-  injectSymbols(context.document)
+  const document = new sketch.Document()
+
+  ButtonExamples.forEach(e => makeSymbol(() => e.component, e.name, document.sketchObject))
+  injectSymbols(document.sketchObject)
+
+  const path = context.scriptPath.split('Contents/Sketch/')[0] + context.command.identifier() + '.sketch'
+
+  document.save(path, err => {
+    if (err) {
+      console.error(err)
+    }
+
+    document.close()
+
+    sketch.Library.getLibraryForDocumentAtPath(path)
+
+    sketch.UI.message('Library added 🙌')
+  })
 }
